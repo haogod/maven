fcc31c3bf8aead40e033d040e31498de5304b2ad
zhli <3293711629@qq.com>
2024-10-11 13:58:44 +0800

    feat(Maven):添加新的文件
    Signed-off-by: zhli <3293711629@qq.com>

---
+++ Maven/src/main/java/com/juvenxu/mvnbook/helloworld/HellowWorld.java
@@ -0,0 +1,17 @@
+package com.juvenxu.mvnbook.helloworld;
+
+
+
+public class HellowWorld{
+    public String sayHello(){
+        return "Hello Maven";
+    }
+
+    public static void main(String []args)
+    {
+       System.out.print(new HellowWorld().sayHello());
+    }
+}
+
+
+
---
+++ Maven/src/test/java/com/juvenxu/mvnbook/helloworld/HelloWorldTest.java
@@ -0,0 +1,7 @@
+package com.juvenxu.mvnbook.helloworld;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class HelloWorldTest {
+}
